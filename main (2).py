d40c52b2c8b10990e340661af1bd69e3d311b8d7
dineshg1437 <144877854+dineshg1437@users.noreply.github.com>
2023-10-13 12:13:26 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,16 +1,48 @@
-class Player:
-  def player(self):
-      print("The player is playing cricket.")
+# Python3 program to calculate the CGPA
+# and CGPA percentage of a student
 
-class Batsman(Player):
-  def play(self):
-    print("The batsman is batting.")
-class Bowler(Player):
-  def play(self):
-    print("The bowler is bowling.")
 
-batsman = Batsman()
-bowler = Bowler()
+def CgpaCalc(marks, n):
 
-batsman.play()
-bowler.play()
+  # Variable to store the grades in
+
+  # every subject
+
+  grade = [0] * n
+
+  # Variables to store CGPA and the
+
+  # sum of all the grades
+
+  Sum = 0
+
+  # Computing the grades
+
+  for i in range(n):
+
+    grade[i] = (marks[i] / 10)
+
+  # Computing the sum of grades
+
+  for i in range(n):
+
+    Sum += grade[i]
+
+  # Computing the CGPA
+
+  cgpa = Sum / n
+
+  return cgpa
+
+
+# Driver code
+
+n = 5
+
+marks = [90, 80, 70, 80, 90]
+
+cgpa = CgpaCalc(marks, n)
+
+print("CGPA = ", '%.1f' % cgpa)
+
+print("CGPA Percentage = ", '%.2f' % (cgpa * 9.5))
